93d297756a26687c0efdd893c5f470e9cffad502
Anson Mansfield <anson.mansfield@gmail.com>
2023-12-06 15:56:38 -0500

    working part 2

--- src/05/seeds.f90
+++ src/05/seeds.f90
@@ -5,7 +5,9 @@ program seeds
   character(len=256) :: buf
 
   integer(kind=8), dimension(:), allocatable :: keys, vals
-
+  integer(kind=8), dimension(:), allocatable :: rkeys, rklen, rvals, rvlen
+  
+  ! write(0,*) "===================================="
   block
     integer :: key_count, buf_len
     integer :: i
@@ -19,12 +21,19 @@ program seeds
     allocate(keys(key_count))
     allocate(vals(key_count))
 
+    allocate(rkeys(key_count/2))
+    allocate(rklen(key_count/2))
+    allocate(rvals(key_count/2))
+    allocate(rvlen(key_count/2))
     
     ! write(0,'("keys: " *(I12))') keys
     ! write(0,'("vals: " *(I12))') vals
     ! write(0,'("buf: ''" A, "''")') buf(:buf_len)
 
     read(buf,*) vals
+
+    rvals(:) = vals(1::2)
+    rvlen(:) = vals(2::2)
 
     read(*,'(A)',iostat=ios) buf ! skip blank line
     ! write(0,'("blank: ''" A, "''")') trim(buf)
@@ -44,6 +53,8 @@ program seeds
     keys = vals
     ! write(0,'("value: " *(I0," "))') keys
 
+    rkeys = rvals
+    rklen = rvlen
     each_line: do
     block
       integer(kind=8) :: v, k, l
@@ -58,12 +69,79 @@ program seeds
 
       read(buf,*) v,k,l
 
-      ! do concurrent (i = lbound(keys,1):ubound(keys,1))
       do i = lbound(keys,1), ubound(keys,1)
+        ! simple point transform
         if (k <= keys(i) .and. keys(i) < k+l) then
           vals(i) = v + (keys(i) - k)
           ! write(0,'("map  : " I0, " -> " I0)') keys(i), vals(i)
         end if
+      end do
+
+      do i = lbound(rkeys,1), ubound(rkeys,1)
+        ! range transform
+        if (rkeys(i) + rklen(i) <= k) then
+          ! ---
+          !     ---
+          ! no intersection
+
+        else if (rkeys(i) >= k+l) then
+          !     ---
+          ! ---
+          ! no intersection
+
+        else if (rkeys(i) >= k .and. rkeys(i) + rklen(i) <= k+l) then
+          ! write(0,*) "case: interior"
+          !   ---
+          ! -------
+          ! fully contained, no need to split or change length
+          rvals(i) = v + rkeys(i) - k
+
+        else if (rkeys(i) < k .and. rkeys(i) + rklen(i) > k+l) then
+          ! write(0,*) "case: exterior"
+          ! -------
+          !   ---
+          ! need to carve into three pieces
+
+          ! insert tail piece into keys
+          rkeys = [rkeys, k + l]
+          rklen = [rklen, rkeys(i) + rklen(i) - k - l]
+          ! insert transformed center piece and default tail piece into vals
+          rvals = [rvals, v, k + l]
+          rvlen = [rvlen, l, rkeys(i) + rklen(i) - k - l]
+          ! trim length of current piece to end before excised piece
+          rklen(i) = k - rkeys(i)
+          rvlen(i) = k - rkeys(i)
+
+        else if (rkeys(i) < k) then
+          ! write(0,*) "case: left"
+          ! -----
+          !   -----
+          ! need to carve into two pieces
+          ! insert transformed right piece into vals
+          rvals = [rvals, v]
+          rvlen = [rvlen, rklen(i) - k + rkeys(i) ]
+          ! trim length of current piece to end before excised piece
+          rklen(i) = k - rkeys(i)
+          rvlen(i) = k - rkeys(i)
+
+        else if (rkeys(i) + rklen(i) > k + l) then
+          ! write(0,*) "case: right"
+          !   -----
+          ! -----
+          ! need to carve into two pieces
+          ! insert transformed middle segment
+          rvals = [rvals, v + (rkeys(i) - k)]
+          rvlen = [rvlen, l - (rkeys(i) - k)]
+          ! move head of current piece and truncate length
+          rvlen(i) = (rkeys(i) + rklen(i)) - (k + l)
+          rvals(i) = k + l
+          rklen(i) = (rkeys(i) + rklen(i)) - (k + l)
+          rkeys(i) = k + l
+        else
+          write(0,*) "unknown case!"
+          call exit(1)
+        end if
+
       end do
 
     end block
@@ -73,5 +151,6 @@ program seeds
 
   ! write(0,'("final: " *(I0," "))') vals
   write(*,'("Part 1: " I0)') minval(vals)
+  write(*,'("Part 2: " I0)') minval(rvals)
 
 end program seeds
