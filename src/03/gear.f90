371fd63f976ce27fd3479e4bbc9886407de081eb
Anson Mansfield <anson.mansfield@gmail.com>
2023-12-06 09:29:15 -0500

    clean up warnings

--- src/03/gear.f90
+++ src/03/gear.f90
@@ -2,16 +2,12 @@ program gear
   use iso_c_binding
   implicit none
 
-  integer :: ios, idx
+  integer :: ios
   character(32768), target :: buf
-  integer :: w, h, i, j, k
+  integer :: w, h
   character, dimension(:,:), pointer :: array
 
   logical, dimension(:,:), allocatable :: is_digit, is_special, is_target, is_gear
-  integer :: num
-  logical :: targeting
-  integer, dimension(:), allocatable :: sums
-  integer, dimension(:,:), allocatable :: gear_totals
 
   read(*,'(A)', iostat=ios) buf
   h = 1
@@ -25,86 +21,95 @@ program gear
 
   call c_f_pointer(c_loc(buf), array, [w,h])
 
-  allocate(is_digit(0:w+1,0:h+1))
-  allocate(is_special(0:w+1,0:h+1))
-  allocate(is_target(0:w+1,0:h+1))
-  allocate(is_gear(0:w+1,0:h+1))
-  allocate(sums(h))
-  allocate(gear_totals(w,h))
-  is_digit(:,:) = .false.
-  is_special(:,:) = .false.
-  is_target(:,:) = .false.
-  is_gear(:,:) = .false.
-  sums(:) = 0
-  gear_totals(:,:) = 0
+  block
+    integer :: i, j, k
+    integer :: num
+    logical :: targeting
+    integer, dimension(:), allocatable :: sums
+    integer, dimension(:,:), allocatable :: gear_totals
 
-  do concurrent (j = 1:h)
-    do concurrent (i = 1:w)
-      if (verify(array(i,j), "0123456789") == 0) is_digit(i,j) = .true.
-      if (.not. is_digit(i,j) .and. array(i,j) /= ".") is_special(i,j) = .true.
-      if (array(i,j) == "*") is_gear(i,j) = .true.
+    allocate(is_digit(0:w+1,0:h+1))
+    allocate(is_special(0:w+1,0:h+1))
+    allocate(is_target(0:w+1,0:h+1))
+    allocate(is_gear(0:w+1,0:h+1))
+    allocate(sums(h))
+    allocate(gear_totals(w,h))
+    is_digit(:,:) = .false.
+    is_special(:,:) = .false.
+    is_target(:,:) = .false.
+    is_gear(:,:) = .false.
+    sums(:) = 0
+    gear_totals(:,:) = 0
+
+    do concurrent (j = 1:h)
+      do concurrent (i = 1:w)
+        if (verify(array(i,j), "0123456789") == 0) is_digit(i,j) = .true.
+        if (.not. is_digit(i,j) .and. array(i,j) /= ".") is_special(i,j) = .true.
+        if (array(i,j) == "*") is_gear(i,j) = .true.
+      end do
     end do
-  end do
 
-  ! write(*,*) "Digits:"
-  ! do j = 1, h
-  !   write(*,*) is_digit(1:w,j)
-  ! end do
-  ! write(*,*) "Specials:"
-  ! do j = 1, h
-  !   write(*,*) is_special(1:w,j)
-  ! end do
+    ! write(*,*) "Digits:"
+    ! do j = 1, h
+    !   write(*,*) is_digit(1:w,j)
+    ! end do
+    ! write(*,*) "Specials:"
+    ! do j = 1, h
+    !   write(*,*) is_special(1:w,j)
+    ! end do
 
-  do concurrent (j = 1:h)
-    do concurrent (i = 1:w)
-      if ( is_digit(i,j) .and. ( &
-          is_special(i-1,j-1) .or. is_special(i,j-1) .or. is_special(i+1,j-1) .or. &
-          is_special(i-1,j  ) .or. is_special(i,j  ) .or. is_special(i+1,j  ) .or. &
-          is_special(i-1,j+1) .or. is_special(i,j+1) .or. is_special(i+1,j+1) ) ) then
-        is_target(i,j) = .true.
-      end if
+    do concurrent (j = 1:h)
+      do concurrent (i = 1:w)
+        if ( is_digit(i,j) .and. ( &
+            is_special(i-1,j-1) .or. is_special(i,j-1) .or. is_special(i+1,j-1) .or. &
+            is_special(i-1,j  ) .or. is_special(i,j  ) .or. is_special(i+1,j  ) .or. &
+            is_special(i-1,j+1) .or. is_special(i,j+1) .or. is_special(i+1,j+1) ) ) then
+          is_target(i,j) = .true.
+        end if
+      end do
     end do
-  end do
 
-  ! write(*,*) "Targets:"
-  ! do j = 1, h
-  !   write(*,*) is_target(1:w,j)
-  ! end do
+    ! write(*,*) "Targets:"
+    ! do j = 1, h
+    !   write(*,*) is_target(1:w,j)
+    ! end do
 
-  do concurrent (j = 1:h)
-    k = -1
-    do i = w, 0, -1
-      if (.not. is_digit(i,j)) k = -1
-      if (k < 0 .and. is_digit(i,j)) then
-        k = i
-      end if
+    do concurrent (j = 1:h)
+      k = -1
+      do i = w, 0, -1
+        if (.not. is_digit(i,j)) k = -1
+        if (k < 0 .and. is_digit(i,j)) then
+          k = i
+        end if
 
-      if  (is_target(i,j)) then
-        targeting = .true.
-      end if
+        if  (is_target(i,j)) then
+          targeting = .true.
+        end if
 
-      if (targeting .and. .not. is_digit(i-1,j)) then
-        read(buf((j-1)*w+i:(j-1)*w+k), *) num
-        sums(j) = sums(j) + num
-        targeting = .false.
-      end if
+        if (targeting .and. .not. is_digit(i-1,j)) then
+          read(buf((j-1)*w+i:(j-1)*w+k), *) num
+          sums(j) = sums(j) + num
+          targeting = .false.
+        end if
 
+      end do
     end do
-  end do
 
-  write(*,*) "Part 1:", sum(sums)
+    write(*,*) "Part 1:", sum(sums)
 
-  do j = 1, h
-    do i = 1, w
-      gear_totals(i,j) = gear_total(i,j)
+    do j = 1, h
+      do i = 1, w
+        gear_totals(i,j) = gear_total(i,j)
+      end do
     end do
-  end do
 
-  
-  write(*,*) "Part 2:", sum(gear_totals)
+    
+    write(*,*) "Part 2:", sum(gear_totals)
+  end block
 
 contains
   function gear_total(i,j)
+    implicit none
     integer, intent(in) :: i, j
     integer :: gear_total
     integer :: count, dj
