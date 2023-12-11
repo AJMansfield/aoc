0360737b931fb17702d9d0e10e01be1fe10a36b2
Anson Mansfield <anson.mansfield@gmail.com>
2023-12-11 15:38:50 -0500

    clean up and make blazing fast

--- 2023-fortran/src/10/pipes.f90
+++ 2023-fortran/src/10/pipes.f90
@@ -6,11 +6,13 @@ program pipes
   integer, dimension(2) :: bounds
   character, dimension(:,:), pointer :: array
   
-  integer, dimension(2), parameter :: N=[0,-1], S=[0,1], E=[1,0], W=[-1,0]
+  integer, parameter :: N=1, E=2, S=3, W=4
 
-  integer, dimension(2) :: pos
+  integer :: res1, res2
 
-  integer :: area, perimeter
+  real t1, t2, t3, t4
+
+  ! call cpu_time(t1)
 
   block
     integer :: ios
@@ -27,92 +29,126 @@ program pipes
     call c_f_pointer(c_loc(buf), array, bounds)
   end block
 
-
-  pos = findloc(array, "S")
+  ! call cpu_time(t2)
 
   block
+    integer, dimension(2) :: pos
+    integer :: area, perimeter
     logical :: closed
 
-    call walk_pipe(pos, pos+E, area, perimeter, closed)
+    pos = findloc(array, "S")
+
+    call walk_pipe(pos, E, area, perimeter, closed)
     if (closed) goto 1
-    call walk_pipe(pos, pos+W, area, perimeter, closed)
+    call walk_pipe(pos, W, area, perimeter, closed)
     if (closed) goto 1
-    call walk_pipe(pos, pos+S, area, perimeter, closed)
+    call walk_pipe(pos, S, area, perimeter, closed)
     1 continue
+
+    res1 = perimeter / 2
+    res2 = abs(area) - perimeter/2 + 1 ! subtract out the line width
   end block
 
-  write(*,'("Part 1: " I0)') perimeter / 2
-  write(*,'("Part 2: " I0)') area
+  ! call cpu_time(t3)
 
+  write(*,'("Part 1: " I0)') res1
+  write(*,'("Part 2: " I0)') res2
+
+  ! call cpu_time(t4)
+
+  ! write(0,'("read : " F10.6)') t2 - t1
+  ! write(0,'("work : " F10.6)') t3 - t2
+  ! write(0,'("write: " F10.6)') t4 - t3
+  ! write(0,'("total: " F10.6)') t4 - t1
 contains
 
-  subroutine walk_pipe(last_pos, this_pos, area, perimeter, closed)
-    integer, dimension(2), intent(in) :: last_pos, this_pos
+  subroutine walk_pipe(in_pos, in_dir, area, perimeter, closed)
+    integer, dimension(2), intent(in) :: in_pos
+    integer, intent(in) :: in_dir
     integer, intent(out) :: area, perimeter
     logical, intent(out) :: closed
 
-    integer, dimension(2) :: q, p
-    logical :: noerr
-
-    q = last_pos
-    p = this_pos
+    integer, dimension(2) :: p
+    integer d
+    
+    p = in_pos
+    d = in_dir
     area = 0
     perimeter = 0
-    noerr = .true.
     closed = .false.
 
     do
-      if (any(p <= 0) .or. any(p > bounds)) closed = .false.
-      if (closed .or. .not. noerr) exit
-
       perimeter = perimeter + 1
-      area = area + p(1) * (p(2) - q(2)) ! Using greenes theorem to compute the area
-
-      select case (array(p(1),p(2)))
-      case ("|")
-        call try_walks(q, p, N, S, noerr)
-      case ("-")
-        call try_walks(q, p, E, W, noerr)
-      case ("L")
-        call try_walks(q, p, N, E, noerr)
-      case ("J")
-        call try_walks(q, p, N, W, noerr)
-      case ("7")
-        call try_walks(q, p, S, W, noerr)
-      case ("F")
-        call try_walks(q, p, S, E, noerr)
-      case (".")
-        noerr = .false.
-      case ("S")
-        closed = .true.
-      case default
-        write(0,*) "unknown case in walk"
+      select case (d)
+      case (N)
+        area = area - p(1) ! greene's theorem
+        p(2) = p(2) - 1
+        if (p(2) < 1) exit
+        select case (array(p(1),p(2)))
+        case ("|")
+          d = N
+        case ("7")
+          d = W
+        case ("F")
+          d = E
+        case ("S")
+          closed = .true.
+          exit
+        case default
+          exit
+        end select
+      case (S)
+        area = area + p(1) ! greene's theorem
+        p(2) = p(2) + 1
+        if (p(2) > bounds(2)) exit
+        select case (array(p(1),p(2)))
+        case ("|")
+          d = S
+        case ("J")
+          d = W
+        case ("L")
+          d = E
+        case ("S")
+          closed = .true.
+          exit
+        case default
+          exit
+        end select
+      case (E)
+        p(1) = p(1) + 1
+        if (p(1) > bounds(1)) exit
+        select case (array(p(1),p(2)))
+        case ("-")
+          d = E
+        case ("J")
+          d = N
+        case ("7")
+          d = S
+        case ("S")
+          closed = .true.
+          exit
+        case default
+          exit
+        end select
+      case (W)
+        p(1) = p(1) - 1
+        if (p(1) < 1) exit
+        select case (array(p(1),p(2)))
+        case ("-")
+          d = W
+        case ("L")
+          d = N
+        case ("F")
+          d = S
+        case ("S")
+          closed = .true.
+          exit
+        case default
+          exit
+        end select
       end select
     end do
-
-    area = abs(area) - perimeter/2 + 1 ! subtract out the line width
-
   end subroutine
 
-  subroutine try_walks(q, p, dp1, dp2, success)
-    integer, dimension(2), intent(inout) :: q, p
-    integer, dimension(2), intent(in) :: dp1, dp2
-    logical, intent(out), optional :: success
-    integer, dimension(2) :: oldp
-
-    oldp = p
-
-    if (all(p + dp1 == q)) then
-      p = p + dp2
-      success = .true.
-    else if (all(p + dp2 == q)) then
-      p = p + dp1
-      success = .true.
-    else
-      success = .false.
-    end if
-
-    if (success) q = oldp
-  end subroutine
 
 end program pipes
