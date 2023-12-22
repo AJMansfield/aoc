3ab58f856e8262c2c2f7c6d37e33c8733ce95d48
Anson Mansfield <anson.mansfield@gmail.com>
2023-12-21 22:56:37 -0500

    need to do some kind of result caching

--- 2023-fortran/src/12/springs.F90
+++ 2023-fortran/src/12/springs.F90
@@ -10,20 +10,15 @@ program springs
 
   integer(Kres) :: res1, res2
 
-  real :: tcb, tce, tctotal
-  real :: trb, tre, trtotal
 #if defined PERF_TIME
   real :: t1, t2, t3, t4
-  call cpu_time(t1)
 #endif
-  tctotal=0
-  trtotal=0
 
 main: block
   integer :: h
   integer, dimension(max_h) :: n, w
-  integer(Kseg), dimension(max_n, max_h) :: segments
-  integer(Kstat), dimension(max_w, max_h) :: status
+  integer(Kseg), dimension(max_n, max_h), target :: segments
+  integer(Kstat), dimension(max_w, max_h), target :: status
   
   h = -1
   w = -1 ! debug sentinels -- should never see a negative anywhere else
@@ -81,8 +76,8 @@ part1: block
 
 end block part1
 part2: block
-  integer(Kseg), dimension(max_n*unfold_by) :: big_segment
-  integer(Kstat), dimension((max_w+1)*unfold_by-1) :: big_status
+  integer(Kseg), dimension(max_n*unfold_by), target :: big_segment
+  integer(Kstat), dimension((max_w+1)*unfold_by-1), target :: big_status
 
   integer :: i, j, lb, ub, big_w, big_n
 
@@ -106,15 +101,9 @@ part2: block
     ! write(0, '("s: " *(I2))') big_segment(:big_n)
     ! write(0, '("a: " *(I1))') big_status(:big_w)
     
-    call cpu_time(tcb)
-    trb = tcb
     res2 = res2 + number_of_matches(big_status(:big_w), big_segment(:big_n))
-    call cpu_time(tce)
-    tre = tce
-    tctotal = tctotal + (tce - tcb)
-    trtotal = trtotal + (tre - trb)
     
-    write(0, '("i: " I4 " r:" I20 " tc:" F10.6 " tr:" F10.6)') i, res2, tctotal, trtotal
+    write(0, '("i: " I4 " r:" I20)') i, res2
 
   end do
 
@@ -148,15 +137,12 @@ end block main
 contains
 
 recursive function number_of_matches(status, segments) result(num)
-  integer(Kstat), dimension(:), intent(in) :: status
-  integer(Kseg), dimension(:), intent(in) :: segments
+  integer(Kstat), dimension(:), intent(in), pointer :: status
+  integer(Kseg), dimension(:), intent(in), pointer :: segments
   integer(Kres) :: num
   integer(Kres) :: n
 
   integer :: i, ub
-
-  call cpu_time(tce)
-  tctotal = tctotal + (tce - tcb)
 
   
   ! write(0, '("begin number_of_matches()")') 
@@ -178,7 +164,6 @@ recursive function number_of_matches(status, segments) result(num)
   ! write(0, '("i= " I0 ", " I0)') 1, ub
   num = 0
   do i = 1, ub
-    call cpu_time(tcb)
     n = number_of_matches_here(status(i:), segments)
     ! write(0, '("n[" I1 "]= " I0)') i, n
     num = num + n
@@ -190,14 +175,12 @@ recursive function number_of_matches(status, segments) result(num)
 end function number_of_matches
 
 recursive function number_of_matches_here(status, segments) result(num)
-  integer(Kstat), dimension(:), intent(in) :: status
-  integer(Kseg), dimension(:), intent(in) :: segments
+  integer(Kstat), dimension(:), intent(in), pointer :: status
+  integer(Kseg), dimension(:), intent(in), pointer :: segments
   integer(Kres) :: num
 
   integer :: seg
   integer :: lb
-  call cpu_time(tce)
-  tctotal = tctotal + (tce - tcb)
 
   ! write(0, '("@s= " I0)') size(segments)
 
@@ -218,9 +201,17 @@ recursive function number_of_matches_here(status, segments) result(num)
 
   lb = seg + 2
   
-  call cpu_time(tcb)
   num = number_of_matches(status(lb:), segments(2:))
   ! write(0, '("n: (rec)" I0)') num
 end function number_of_matches_here
+
+function cache_key(status, segments) result(k)
+  integer(Kstat), dimension(:), intent(in), pointer :: status
+  integer(Kseg), dimension(:), intent(in), pointer :: segments
+  integer(8) :: k
+
+  k = loc(status)
+
+end function
   
 end program springs
